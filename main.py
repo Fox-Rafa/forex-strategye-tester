7703526a6788586fe0b7139315ff25bc343b479e
Rafael Fox <Fox-Rafa@users.noreply.github.com>
2020-08-13 09:27:10 -0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,9 @@
+import pandas as pd
+import colour
+
+class data:
+    def __init__(self, df):
+        self = df
+    def(n, color):
+        return(prices.rolling(window=n).mean(), Color(color).hex)
+
